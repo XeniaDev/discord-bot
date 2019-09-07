c76c09c3baccaac648ca52f82fe3a7fda3960f53
haruyuki <jumpy12359@gmail.com>
2019-09-08 00:48:43 +1000

    Update files

--- bayo.js
+++ bayo.js
@@ -1,50 +1,13 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.on('message',message=>{
+  if (message.content == 'hi'){
+   message.reply('wait am i working?');
+  }
+  if (message.content == 'bayo!'){
+   message.reply('call me Bayonetta!');
+  }
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `bayo!`
-    if (message.substring(0, 1) == 'bayo!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'hi':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hi!'
-                });
-            break;
-            case 'turkey':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Ben şimdi türkçe konuşuyorum!'
-                });
-            break;
-            case 'italy':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Ciao amici italiani!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+Client.login(process.env.BOT_TOKEN);
