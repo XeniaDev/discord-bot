548f5ebbfd2326aa0fd08d24a63528a51de71455
ハルユキ <jumpy12359@gmail.com>
2019-09-08 00:51:03 +1000

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -8,6 +8,9 @@ Client.on('message',message=>{
   if (message.content == 'bayo!'){
    message.reply('call me Bayonetta!');
   }
+  if (message.content == 'heroku?'){
+    message.reply('Yes I\'m running off Heroku!');
+  }
 });
 
 Client.login(process.env.BOT_TOKEN);
