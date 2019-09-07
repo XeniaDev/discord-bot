72c4b5054a45b07d2de1a587e8fb00a580734992
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-07 19:44:46 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const Client = new Discord.Client();
 
 Client.on('message',message=>{
-  if (message.startsWith("bayo!")){
   if (message.content == 'bayo!hi'){
    message.reply('wait am i working?');
   }else if (message.content == 'bayo!'){
@@ -13,10 +12,7 @@ Client.on('message',message=>{
     message.reply('Thanks for waiting for me ergastolator1!');
   }else if (message.content == 'bayo!help'){
     message.reply("I'm a very basic bot made by BayoDino and Haruyuki and Ergastolator1. I'll become a helper for Cranterns bot ^-^, I'm not a person that made it, BTW :slight_smile: I only helped to make it, but thanks for including me anyways in the command!");
-  } else {
-    message.reply("What?");
   }
-}
 });
 
 Client.login(process.env.BOT_TOKEN);
