86253dd90b57b846b589a35f6f505e07ee4fba7e
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-07 19:43:55 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -12,7 +12,7 @@ Client.on('message',message=>{
   }else if (message.content == 'bayo!ergastolator1'){
     message.reply('Thanks for waiting for me ergastolator1!');
   }else if (message.content == 'bayo!help'){
-    message.reply("I'm a very basic bot made by BayoDino and Haruyuki and Ergastolator1. I'll become a helper for Cranterns bot ^-^");
+    message.reply("I'm a very basic bot made by BayoDino and Haruyuki and Ergastolator1. I'll become a helper for Cranterns bot ^-^, I'm not a person that made it, BTW :slight_smile: I only helped to make it, but thanks for including me anyways in the command!");
   } else {
     message.reply("What?");
   }
