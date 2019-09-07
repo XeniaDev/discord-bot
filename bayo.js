54a1b6a78f4bdb4a92e5c2f2c1037b7b1110323f
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-07 19:24:07 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -11,6 +11,9 @@ Client.on('message',message=>{
   if (message.content == 'heroku?'){
     message.reply('Yes I\'m running off Heroku!');
   }
+  if (message.content == 'ergastolator1'){
+    message.reply('Thanks for waiting for me!');
+  }
 });
 
 Client.login(process.env.BOT_TOKEN);
