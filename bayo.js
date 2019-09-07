386c75265ace94abfb3067734f37959e7d229b77
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-07 19:49:28 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -14,7 +14,10 @@ Client.on('message',message=>{
     message.reply("I'm a very basic bot made by BayoDino and Haruyuki and Ergastolator1. I'll become a helper for Cranterns bot ^-^, Ergastolator1 is not a person that made it, BTW :) He only helped to make it, but thanks for including him anyways in the command!");
   }else if (message.content == 'bayo!confused'){
     message.reply("Why are you confused?");
+  }else if (message.content == 'bayo!ily'){
+    message.reply("oooh thank you!");
   }
+
 });
 
 Client.login(process.env.BOT_TOKEN);
