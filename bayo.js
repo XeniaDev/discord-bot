067bcadb86dd58fcb25be4063ff33cc6007fbaff
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-07 19:29:42 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -2,19 +2,19 @@ const Discord = require('discord.js');
 const Client = new Discord.Client();
 
 Client.on('message',message=>{
-  if (message.content == 'bayo! hi'){
+  if (message.content == 'bayo!hi'){
    message.reply('wait am i working?');
   }
   if (message.content == 'bayo!'){
    message.reply('call me Bayonetta!');
   }
-  if (message.content == 'bayo! heroku?'){
+  if (message.content == 'bayo!heroku?'){
     message.reply('Yes I\'m running off Heroku!');
   }
-  if (message.content == 'bayo! ergastolator1'){
+  if (message.content == 'bayo!ergastolator1'){
     message.reply('Thanks for waiting for me ergastolator1!');
   }
-  if (message.content == 'bayo! help'){
+  if (message.content == 'bayo!help'){
     message.reply('I/'m a very basic bot made by BayoDino and Haruyuki and Ergastolator1. I/'ll become a helper for Cranterns bot ^-^');
   }
 });
