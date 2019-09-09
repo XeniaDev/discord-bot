ae314f854dcf0171e1ec1d4a0d6b1a491550c1d5
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-10 00:04:13 +0430

    Update bayo.js

--- bayo.js
+++ bayo.js
@@ -138,7 +138,20 @@ Client.on('message', message => {
         let reverseArray = splitMessage.reverse();
         let joinArray = reverseArray.join("");
         message.channel.send(joinArray);
-    }
+    }else if (message.content == 'bayo!adventure') {
+        // Send the user's avatar URL
+         var adventures = Array(
+            " Tried to Take a look at The forest, Nice but burned forest",
+            " Looked up at BayoDino, she saw a dragon, Both are running away.",
+            " Tried to Help Haru to climb the tree, But you couldn't and you and he fell down. ouch!",
+            " Going to collect bunch of logs. He/She saw Khaos cannot find logs, gave the logs to her. so kindness.",
+            " Found HedaFox but he was crying. He/She tried to ask HedaFox what's going on. but he went away.",
+            " Suggested CayoDino to play with him. But he didn't accepted because his brother didn't allowed him, aww.",
+            " Was running but he/she didn't see Ergastolator and they fell down, it just was an small mistake."
+        );
+        var adventure = adventures[Math.floor(Math.random() * adventures.length)];
+        message.reply("" + adventure + "");
+        }
 });
 
 Client.login(process.env.BOT_TOKEN);
