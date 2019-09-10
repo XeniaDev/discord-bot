6d821101cb62b9b9da699ffabc42b6828bcd1371
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-10 19:36:32 +0430

    Update element.js

--- commands/element.js
+++ commands/element.js
@@ -5,6 +5,17 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   //get infos
     message.channel.send(`Blood`,{ files: ["https://i.postimg.cc/L6HnKHjw/blood.png"]});
   }
+  message.channel.send({
+    embed: {
+      author: {
+        name: message.author.username,
+        icon_url: message.author.avatarURL
+      },
+      color: 0x0000FF,
+      title: "User Info : " + message.author.username,
+      description: "Username:" + message.author.username + "\n User ID: " + message.author.id + "\n Status: " + message.author.presence.status + ""
+    }
+  });
 };
 
 exports.conf = {
