822adadef11149ec3255af1ef8038875d024e069
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-10 11:18:47 +0430

    Update adventure.js

--- commands/adventure.js
+++ commands/adventure.js
@@ -1,11 +1,11 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const adventures = Array(" Tried to Take a look at The forest, Nice but burned forest",
+  const adventures = Array(" tried to take at look at the forest, but it's a burned one.",
     " Looked up at BayoDino, she saw a dragon, Both are running away.",
-    " Tried to Help Haru to climb the tree, But you couldn't and you and he fell down. ouch!",
+    " tried to help Haru climb the tree, but they couldn't and they fell down. Ouch!",
     " Going to collect bunch of logs. He/She saw Khaos cannot find logs, gave the logs to her. so kindness.",
-    " Found HedaFox but he was crying. He/She tried to ask HedaFox what's going on. but he went away.",
+    "  found HedaFox crying, and tried to ask him what's going on, but he went away.",
     " Suggested CayoDino to play with him. But he didn't accepted because his brother didn't allowed him, aww.",
-    " Was running but he/she didn't see Ergastolator and they fell down, it just was an small mistake."
+    " was running, but they didn't see Ergastolator and they fell down. It was just a small mistake."
   );
   const adventure = adventures[Math.floor(Math.random() * adventures.length)];
   message.reply(adventure);
