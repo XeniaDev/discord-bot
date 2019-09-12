86cf2bddacd6fd73ad14fe5aae1ddcaedcdab78b
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-12 14:40:13 +0430

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  let messagecount = parseInt(args);
+message.channel.fetchMessages({ limit: messagecount })
+  .then(messages => message.channel.bulkDelete(messages));
+};
+
+exports.conf = {
+  enabled: true, // Whether this command is enabled
+  guildOnly: false, // If the command can only be run in servers (No PM's)
+  aliases: [], // Other ways to call the command
+  permLevel: "User" // The minimum permission needed to run the command
+};
+
+exports.help = {
+  name: "ily", // The name of the command used to call it
+  category: "Fun", // The category the command fits in
+  description: "I'll be thankful!", // Short description of what the command does
+  usage: "ily" // An example of how to use the command
+};
