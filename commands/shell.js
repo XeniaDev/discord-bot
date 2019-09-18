7f111abf3b36fb4017f8d19a257365c80179a414
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-18 13:49:30 +0430

    Create shell.js

---
+++ commands/shell.js
@@ -0,0 +1,25 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  if (!args.length) {
+    return;
+  } else {
+  const dollar = (args[0] / 1000000);
+  const frgem = (args[0] / 10000);
+  const frtr = (args[0] / 10);
+  const cs = (args[0] * 0.00000016); //1 == 0.00000016
+    message.channel.send(args[0]+" Worths: \n "+dollar+" USD \n "+frgem+" FR Gems \n "+frtr+" FR Treasure \n "+cs+" C$");
+  }
+};
+
+exports.conf = {
+  enabled: true, // Whether this command is enabled
+  guildOnly: false, // If the command can only be run in servers (No PM's)
+  aliases: [], // Other ways to call the command
+  permLevel: "User" // The minimum permission needed to run the command
+};
+
+exports.help = {
+  name: "diamond", // The name of the command used to call it
+  category: "Informational", // The category the command fits in
+  description: "Getting ratio from usd, Flight rising gems, Flight rising tr and C$ with diamonds", // Short description of what the command does
+  usage: "diamond" // An example of how to use the command
+};
