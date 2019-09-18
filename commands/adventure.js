6952742da269276179229b7748d392bacc27769c
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-18 20:50:38 +0430

    Update adventure.js

--- commands/adventure.js
+++ commands/adventure.js
@@ -1,13 +1,17 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const beebs = ["Haru","Ergastolator","HedaFox","CayoDino","Seraph","Khaos","BayoDino"];
+  const pron = {"Haru": "he", "Ergastolator": "he", "HedaFox": "he","CayoDino": "he", "Seraph": "she", "Khaos": "she", "BayoDino": "she"};
+  const pron2 = {"Haru": "him", "Ergastolator": "him", "HedaFox": "him","CayoDino": "him", "Seraph": "her", "Khaos": "her", "BayoDino": "her"};
+  const beeb = beebs[Math.floor(Math.random() * beebs.length)];
   const adventures = Array(" tried to take at look at the forest, but it's a burned one.",
-      " Looked up at BayoDino, but She saw a big dragon, Both tried to runaway and they could.",
-    " tried to help Haru climb the tree, but they couldn't and they fell down. Ouch!",
-    " Went to collect bunch of logs. They saw Khaos that she cannot find logs, they gave the logs to her. so kind.",
-    "  found HedaFox crying, and tried to ask him what's going on, but he went away.",
-    " Suggested CayoDino to play with him. But he didn't accept because his brother didn't allow him, Ah.",
-    " was running, but they didn't see Ergastolator and they fell down. It was just a small mistake."
+" Looked up at "+beeb+", but "+pron[beeb]+" saw a big dragon, Both tried to runaway and they could.",
+    " tried to help "+beeb+" climb the tree, but they couldn't and they fell down. Ouch!",
+    " Went to collect bunch of logs. Saw "+beeb+" that "+pron[beeb]+" cannot find logs, they gave the logs to "+pron2[beeb]+". so kind.",
+    "  found "+beeb+" crying, and tried to ask "+pron[beeb]+" what's going on, but he went away.",
+    " Suggested "+beeb+" to play with "+pron2[beeb]+". But "+pron[beeb]+" didn't accept because their brother didn't allow "+pron2[beeb]+", Ah.",
+    " was running, but they didn't see "+beeb+" and they fell down. It was just a small mistake."
   );
-  const adventure = adventures[Math.floor(Math.random() * adventures.length)];
+  var adventure = adventures[Math.floor(Math.random() * adventures.length)];
   message.reply(adventure);
 };
 
