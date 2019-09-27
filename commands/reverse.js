1e79861273764da600b585fa08b232bd9f0a3a71
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-09-27 13:52:27 +0330

    Create reverse.js

---
+++ commands/reverse.js
@@ -0,0 +1,21 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const combinedString = args.join(" ");
+  const splitMessage = combinedString.split("");
+  const reverseArray = splitMessage.reverse();
+  const joinArray = reverseArray.join("");
+  message.channel.send(joinArray);
+};
+
+exports.conf = {
+  enabled: true, // Whether this command is enabled
+  guildOnly: false, // If the command can only be run in servers (No PM's)
+  aliases: [], // Other ways to call the command
+  permLevel: "User" // The minimum permission needed to run the command
+};
+
+exports.help = {
+  name: "reverse", // The name of the command used to call it
+  category: "Fun", // The category the command fits in
+  description: "Super reverse the message that you typed", // Short description of what the command does
+  usage: "reverse <...message>" // An example of how to use the command
+};
