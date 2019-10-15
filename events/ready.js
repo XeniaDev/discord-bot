1a86d40f06103da770fed68f8bb91b129e06a0b4
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-10-15 15:44:57 +0330

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,5 +7,6 @@ module.exports = async client => {
   client.user.setStatus("online");
 
   const channel = client.channels.get('619902581342208014');
-  channel.send("I won't send eyes again! Thank you for also posting eyes :eyes:");
+  //channel.send("I won't send eyes again! Thank you for also posting eyes :eyes:");
+  console.log("ready.");
 };
