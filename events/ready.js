d14d2b4b8bbd6ce05df157cdc0cadde2ebf352d7
EchoDino <49869460+EchoDino@users.noreply.github.com>
2019-10-15 15:47:25 +0330

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`bayo!help`, {type: "PLAYING"});
+  client.user.setActivity(`to BayoDino, bayo!help for help!`, {type: "Listening"});
   client.user.setStatus("online");
 
   const channel = client.channels.get('619902581342208014');
