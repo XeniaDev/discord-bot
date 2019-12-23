88e34dabae32a10d025a84c8372de2969b4b4ee3
Sabah Demir <49869460+EchoDino@users.noreply.github.com>
2019-12-23 22:00:02 +0330

    Create action.js

---
+++ commands/action.js
@@ -0,0 +1,62 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const foods = Array(
+    ":bug:",
+    ":ant:",
+    ":bee:",
+    ":scorpion:",
+    ":snake:",
+    ":cactus:",
+    ":four_leaf_clover: ",
+    ":pineapple:",
+    ":corn:",
+    ":lollipop:",
+    ":cheese:",
+    ":pizza:",
+    ":stew:",
+    ":avocado:",
+    ":dango:",
+    ":tomato:",
+    ":egg:",
+    ":potato:",
+    ":baby_bottle:",
+    ":beer:",
+    ":rat:",
+    ":christmas_tree:",
+    ":chestnut:",
+    ":lizard:",
+    ":shrimp:",
+    ":tangerine:"
+  );
+  const food = foods[Math.floor(Math.random() * foods.length)];
+  if (args.length === 0){ 
+    message.reply("You cannot feed air");
+    return;
+  }
+
+  if (args[0].startsWith('<@') && args[0].endsWith('>')) {
+    args = args[0].slice(2, -1);
+
+    if (args.startsWith('!')) {
+        args = args.slice(1);
+    }
+if (args[0] == "<@619791583805440000>"){ 
+    message.reply("*Runs Away...*");
+    return;
+  }
+    message.reply(" **You put a "+food+" in "+client.users.get(args)+"'s mouth!**");
+  }
+};
+
+exports.conf = {
+  enabled: true, // Whether this command is enabled
+  guildOnly: false, // If the command can only be run in servers (No PM's)
+  aliases: ["act"], // Other ways to call the command
+  permLevel: "User" // The minimum permission needed to run the command
+};
+
+exports.help = {
+  name: "action", // The name of the command used to call it
+  category: "Fun", // The category the command fits in
+  description: "Do something weird with who you mention!", // Short description of what the command does
+  usage: "action" // An example of how to use the command
+};
